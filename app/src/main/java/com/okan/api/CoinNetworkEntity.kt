96fe5aa3c70e4b9a0fd63f61afa596485e7794f2
Okan Serdaroğlu <okanserdaroglu2@gmail.com>
2020-09-05 21:01:31 +0300

    Coin Model For Project Level And API Level

---
+++ app/src/main/java/com/okan/api/CoinNetworkEntity.kt
@@ -0,0 +1,21 @@
+package com.okan.api
+
+import com.google.gson.annotations.Expose
+import com.google.gson.annotations.SerializedName
+
+data class CoinNetworkEntity(
+
+    @SerializedName("id")
+    @Expose
+    val id: String,
+
+    @SerializedName("symbol")
+    @Expose
+    val symbol: String,
+
+    @SerializedName("name")
+    @Expose
+    val name: String,
+
+)
+
