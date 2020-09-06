735a2382ab2b50c90670fd6034097c6fbfec651d
Okan Serdaroğlu <okanserdaroglu2@gmail.com>
2020-09-06 18:24:50 +0300

    AuthRepository And AuthViewModel

--- app/src/main/java/com/okan/repository/auth/AuthRepository.kt
+++ app/src/main/java/com/okan/repository/auth/AuthRepository.kt
@@ -1,21 +1,35 @@
 package com.okan.repository.auth
 
 import com.google.firebase.auth.FirebaseAuth
+import com.okan.utils.DataState
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.launch
 
 class AuthRepository
 constructor(
     private val firebaseAuth: FirebaseAuth
 ) {
-    suspend fun loginWithEmail(email: String, password: String) {
+    suspend fun loginWithEmail(
+        email: String,
+        password: String
+    ): Flow<DataState<Boolean>> = flow {
+
+        emit(DataState.Loading)
         firebaseAuth.signInWithEmailAndPassword(email, password)
             .addOnCompleteListener { task ->
-                if (task.isSuccessful) {
-
-
-                } else {
-
+                GlobalScope.launch(Dispatchers.IO) {
+                    if (task.isSuccessful) {
+                        emit(DataState.Success(true))
+                    } else {
+                        emit(DataState.Error(task.exception!!))
+                    }
                 }
             }
+
     }
 
 }
--- app/src/main/java/com/okan/ui/auth/AuthViewModel.kt
+++ app/src/main/java/com/okan/ui/auth/AuthViewModel.kt
@@ -1,9 +1,15 @@
 package com.okan.ui.auth
 
 import androidx.hilt.lifecycle.ViewModelInject
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.okan.repository.auth.AuthRepository
+import com.okan.ui.auth.AuthStateEvent.Login
+import com.okan.ui.auth.AuthStateEvent.None
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.launch
 
 @ExperimentalCoroutinesApi
 class AuthViewModel
@@ -12,6 +18,27 @@ constructor(
     private val authRepository: AuthRepository
 ) : ViewModel() {
 
+    private val _dataState: MutableLiveData<Boolean> = MutableLiveData()
+
+    val dataState: LiveData<Boolean>
+        get() = _dataState
+
+    fun setStateEvent(authStateEvent: AuthStateEvent) {
+        viewModelScope.launch {
+            when (authStateEvent) {
+                is Login -> {
+                    authRepository.loginWithEmail(
+                        email = authStateEvent.email,
+                        password = authStateEvent.password
+                    )
+                }
+
+                is None -> {
+
+                }
+            }
+        }
+    }
 
 }
 
